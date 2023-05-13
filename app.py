969e554340b36bc3fe1a274e2dbb903db0f85fc4
Taweesab <bossbanthao@gmail.com>
2023-05-14 00:48:34 +0700

    modify app.py

--- app.py
+++ app.py
@@ -111,9 +111,4 @@ mode = "dev"
 
 if __name__ == '__main__':
     # Start the Flask application
-    if mode == "dev":
-        app.run(host='0.0.0.0', port=8000, debug=True)
-    else:
-        serve(app, host='0.0.0.0', port=8000, threads=1)
-
-# index()
+    app.run(debug=True)
